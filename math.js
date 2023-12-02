b82c7f75dbbe647e6336a85e92e4fbff314ae0c0
LuizBlume <luiz.fernando.251107@gmail.com>
2023-12-02 13:58:45 -0300

    calculator finish

--- math.js
+++ math.js
@@ -1,48 +1,18 @@
-let valor1 = document.querySelector("#valor1").value;
-let valor2 = document.querySelector("#valor2").value;
-let soma = document.querySelector("#btSoma");
-let subtracao = document.querySelector("#btSub");
-let multiplicacao = document.querySelector("#btMult");
-let divisao = document.querySelector("#btDiv");
-let resultado = document.querySelector("#resultado");
-
-function somar(a, b) {
-    let soma = a + b;
-    resultado.innerHTML = soma.toFixed(7);
-}
-function subtrair(a, b) {
-    let subtracao = a - b;
-    resultado.innerHTML = subtracao.toFixed(7);
-}
-function multiplicar(a, b) {
-    let multiplicacao = a * b;
-    resultado.innerHTML = multiplicacao.toFixed(7);
-}
-function dividir(a, b) {
-    let divisao = a / b;
-    resultado.innerHTML = divisao.toFixed(7);
+function inserirNumero(num) {
+    document.querySelector('.resultado').innerHTML += num;
 }
 
-soma.addEventListener("click", function() {
-    let valor1 = document.querySelector("#valor1").value;
-    let valor2 = document.querySelector("#valor2").value;
-    somar(Number(valor1), Number(valor2));
-});
+function limparTela() {
+    document.querySelector('.resultado').innerHTML = '';
+}
 
-subtracao.addEventListener("click", function() {
-    let valor1 = document.querySelector("#valor1").value;
-    let valor2 = document.querySelector("#valor2").value;
-    subtrair(Number(valor1), Number(valor2));
-});
-
-multiplicacao.addEventListener("click", function() {
-    let valor1 = document.querySelector("#valor1").value;
-    let valor2 = document.querySelector("#valor2").value;
-    multiplicar(Number(valor1), Number(valor2));
-});
-
-divisao.addEventListener("click", function() {
-    let valor1 = document.querySelector("#valor1").value;
-    let valor2 = document.querySelector("#valor2").value;
-    dividir(Number(valor1), Number(valor2));
-});
+function calculate() {
+    const expressao = document.querySelector('.resultado').innerHTML;
+    // Lembrar: Try | catch será usada para realizar ou bloquear uma ação, nesse caso será usado para permitir a conta, ou bloquear caso o usuário utilize algarismos que não irá funcionar na calculadora.
+    try {
+        let resultado = eval(expressao); // Lembrar: Eval será usada para que o sistema receba o sinal da conta, e aplicará na conta descrita pelo usuário
+        document.querySelector('.resultado').innerHTML = resultado;
+    } catch (error) {
+        document.querySelector('.resultado').innerHTML = 'Erro';
+    }
+}
